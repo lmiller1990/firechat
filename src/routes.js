f13c2cf655aa83de53fe537f97cf27caa300848e
lachlan <lachlan.miller.1990@outlook.com>
2017-10-17 22:04:07 +0900

    Restructure files

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
-import Home from './components/Home.vue'
-import Signin from './components/Signin.vue'
-import Signup from './components/Signup.vue'
-import Chat from './components/Chat.vue'
+import Home from './components/home/Home.vue'
+import Signin from './components/home/Signin.vue'
+import Signup from './components/home/Signup.vue'
+import Chat from './components/chat/Chat.vue'
 
 export default [
   {
