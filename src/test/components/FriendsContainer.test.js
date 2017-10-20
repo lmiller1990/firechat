c671909dbac3a5590bc0a819102d80cfe8d6acab
lachlan <lachlan.miller.1990@outlook.com>
2017-10-20 13:10:55 +0900

    Improve test

--- src/test/components/FriendsContainer.test.js
+++ src/test/components/FriendsContainer.test.js
@@ -1,8 +1,40 @@
-import { mount } from 'vue-test-utils'
-import SidebarFriendsContainer from '@/components/chat/sidebar/SidebarConversationItem'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { shallow } from 'vue-test-utils'
+import SidebarFriendsContainer from '@/components/chat/sidebar/SidebarFriendsContainer'
+Vue.use(Vuex)
 
 describe('FriendsContainer', () => {
+  let store
+
+  beforeEach(() => {
+    store = new Vuex.Store({
+      modules: {
+        users: {
+          namespaced: true,
+          state: {
+            all: {
+              '0': { id: 'def' }, '1': { id: 'abc' }
+            },
+          },
+          getters: {
+            allUserIdsExceptCurrent: () => ['0', '1']
+          }
+        }
+      }
+    })
+  })
+
   it('renders a list of friends', () => {
-    // const wrapper = mount(SidebarFriendsContainer)
+    const wrapper = shallow(SidebarFriendsContainer, {
+      store,
+      stubs: {
+        SidebarUserItem: '<SidebarUserItem />'
+      }
+    })
+
+    console.log(wrapper.html())
+    expect(wrapper.html().includes('sidebaruseritem')).toBe(true)
+
   })
 })
